fa9b3231af2961c3d5dc5ffc70919b780b2bd572
96neko1st <seed.kira07@gmail.com>
2017-12-12 22:31:29 +0900

    add:機能追加 顔ハメパネル撮影場所と自身の位置から距離を求め、一定範囲だとその場所の顔ハメ画像にする機能を追加。メモリを食っていたバグを修正

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,38 +1,39 @@
 /*撮影に関するscript*/
 
-//videoのidを取得する変数
-var video;
 //canvas変数
 var canvas;
 var context;
 //画像
 var imgPanelFile = ["./img/panel_1.png","./img/panel_2.png"];
 var imgPanel;
+/*place場所に関する変数
+    場所 0:パルディ 1:学校*/
+var placeLat = [34.252477, 34.253453];
+var placeLng = [132.906987, 132.904284];
 
+//顔をハメる空間を設定する変数
 var position_x = [150,170];
 var position_y = [220,300];
+//円の半径
 var radius = [60,80];
-
-var number;
+var number = 0;
 
 $(document).ready(function(){
     initVideo();
+    //setInterval(getGeolocation, 1000);
     $("#mapButton").click(function(){
-        window.location.href = "map.html"
-    });    
+        window.location.href = "map.html";
+    });
 });
+
+//**  canvasに関する処理  **//
+
 //ビデオの初期化
 function initVideo(){
-    number = sessionStorage.getItem("placeNumber");
-    if (number == null) {
-        number = 0
-    }
-    //背景画像の読み込み
     imgPanel = new Image();
     imgPanel.src = imgPanelFile[number];
-
     //videoの初期化
-    video = document.getElementById("video");
+    var video = document.getElementById("video");
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     window.URL = window.URL || window.webkitURL;
     navigator.getUserMedia({
@@ -50,30 +51,34 @@ function initVideo(){
 function initCanvas(){
     canvas = document.getElementById("dst");
     /*canvasのサイズをスマホサイズに調整*/
-    canvas.width = $(document).width();
-    canvas.height = $(document).height();
+    canvas.width = $(document).width();;
+    canvas.height = $(document).height();;
     context = canvas.getContext("2d");
     /*反転処理*/
     context.translate(canvas.width,0);
     context.scale(-1,1);
+    /* 背景を画像を設定 */
+    context.beginPath();
+    context.drawImage(imgPanel, 0, 0, canvas.width, canvas.height);
     
+    context.beginPath();
+    context.arc(position_x[number], position_y[number], radius[number], 0, Math.PI * 2, false);
+    context.clip();
     setInterval(drawCanvas, 30);
 }
 
 //canvansに顔ハメパネルを描画する
 function drawCanvas(){
-    /* 背景を画像を */
-    context.beginPath();
-    context.drawImage(imgPanel, 0, 0, canvas.width, canvas.height);
-    
     /*顔を当てはめる場所を設定*/
-    context.beginPath();
-    context.arc(position_x[number], position_y[number], radius[number], 0, Math.PI * 2, false);
-    context.clip();
     /*写真を撮影*/
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
 }
 
+
+
+//** 画像を保存する処理　**//
+
+//canvasを画像に変換し、保存する
 function savePhoto(){
     var base64 = canvas.toDataURL("image/png");
     var blob = Base64toBlob(base64);
@@ -120,4 +125,50 @@ function saveBlob(blob, fileName)
     a.download = fileName;
     // イベントの発火
     a.dispatchEvent(event);
+}
+
+//** 顔をハメる写真を決定する **//
+
+//位置情報を取得する
+function getGeolocation() {
+    //対応しているデバイスか判断
+    if (navigator.geolocation) {
+        //緯度経度を取得する
+        navigator.geolocation.getCurrentPosition(function(position) {
+            getImage(position);
+        }, function(error) {
+            // エラー番号に対応したメッセージ
+            var errorInfo = ["原因不明のエラーが発生しました…。", "位置情報の取得が許可されませんでした…。", "電波状況などで位置情報が取得できませんでした…。", "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"];
+            // エラー番号
+            var errorNo = error.code;
+            // エラーメッセージmage();
+            var errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[errorNo];
+            // アラート表示
+            alert(errorMessage);
+        }, {
+            "enableHighAccuracy": false,
+            "timeout": 1000000,
+            "maximumAge": 0,
+        });
+    } else {
+        // エラーメッセージ
+        var errorMessage = "お使いの端末は、GeoLacation APIに対応していません。";
+        // アラート表示
+        alert(errorMessage);
+    }
+}
+
+//自分の位置と顔ハメ名所の距離を測り一定以上だと対応した画像にする
+function getImage(position){
+    //自身の位置情報
+    var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    for (var i = 0; i < imgPanelFile.length; i++) {
+        var pos = [myLatLng, new google.maps.LatLng(placeLat[i], placeLng[i])];
+        var dist = google.maps.geometry.spherical.computeLength(pos);  
+        if (dist < 85) {
+            number = i;
+            imgPanel.src = imgPanelFile[number];
+            return 0;
+        }
+    }
 }
--- www/js/map.js
+++ www/js/map.js
@@ -6,75 +6,78 @@ var syncerWatch = {
     map: null,
     myMarker: null,
 };
+
 /*place場所に関する変数
     場所 0:パルディ 1:学校*/
+var placeName = ["パルディ・大崎上島店","広島商船高等専門学校"]
 var placeLat = [34.252477, 34.253453];
 var placeLng = [132.906987, 132.904284];
 var placeImage = ["./img/icon/school.png", "./img/icon/tokyoskytree.png"];
 
 $(document).ready(function() {
-    setInterval(getGeolocation, 1000);
+    setInterval(getGeolocation, 500);
 });
+
 //初期化してgoogle mapを表示する
-function initMap(_position) {
+function initMap(position) {
     //取得できないならリターン
-    if (_position.coords.latitude == null || _position.coords.longitude == null) {
+    if (position.coords.latitude == null || position.coords.longitude == null) {
         return false;
     }
-    // 位置情報
-    var latlng = new google.maps.LatLng(_position.coords.latitude, _position.coords.longitude);
-    //初期読み込み時
-    if (syncerWatch.map == null) {
-        //地図を表示
-        syncerWatch.map = new google.maps.Map(document.getElementById('map'), {
-            center: latlng,
-            zoom: 15
-        });
-        //自身の位置にMartkerを描画する
-        syncerWatch.myMarker = new google.maps.Marker({
-            position: latlng,
+    //自身の位置情報
+    var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //地図を表示
+    syncerWatch.map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 15
+    });
+    //自身の位置にMartkerを描画する
+    syncerWatch.myMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: syncerWatch.map,
+        animation: google.maps.Animation.BOUNCE
+    });
+    //顔ハメパネルアイコンに関する処理
+    for (var i = 0; i < placeImage.length; i++) {
+        //撮影場所の中心座標
+        var center = new google.maps.LatLng(parseFloat(placeLat[i]), parseFloat(placeLng[i]));
+        //撮影場所にアイコンを置く
+        var placeMarker = new google.maps.Marker({
+            position: center,
             map: syncerWatch.map,
-            animation: google.maps.Animation.BOUNCE
+            icon: placeImage[i]
         });
-        for (var i = 0; i < placeImage.length; i++) {
-            //撮影場所の中心座標
-            var center = new google.maps.LatLng(parseFloat(placeLat[i]), parseFloat(placeLng[i]));
-            //撮影場所にアイコンを置く
-            var placeMarker = new google.maps.Marker({
-                position: center,
-                map: syncerWatch.map,
-                icon: placeImage[i]
-            });
-            //撮影場所を中心として撮影可能範囲を示す円を描画
-            var circle = new google.maps.Circle({
-                map: syncerWatch.map,
-                strokeColor: "red", //線の色
-                fillColor: "purple", //円の塗りつぶし色
-                center: center,
-                radius: 100
-            })
-        }
-    }
-    //二回目以降
-    else {
-        for (var i = 0; i < placeImage.length; i++) {
-            var pos = [latlng, new google.maps.LatLng(placeLat[i], placeLng[i])];
-            var dist = google.maps.geometry.spherical.computeLength(pos);
-            if (dist < 350) {
-                sessionStorage.setItem('placeNumber', i);
-            }
-        }
-        // マーカーの場所を変更
-        syncerWatch.myMarker.setPosition(latlng);
+        //撮影場所を中心として撮影可能範囲を示す円を描画
+        var circle = new google.maps.Circle({
+            map: syncerWatch.map,
+            strokeColor: "red", //線の色
+            fillColor: "purple", //円の塗りつぶし色
+            center: center,
+            radius: 100
+        })
     }
 }
+
+//自身の位置情報を更新する
+function updateMap(position){
+    //自身の位置情報
+    var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // マーカーの場所を変更
+    syncerWatch.myMarker.setPosition(myLatLng);
+}
+
 //位置情報を取得する
 function getGeolocation() {
     //対応しているデバイスか判断
     if (navigator.geolocation) {
-        //緯度経度を取得し、initMao関数呼び出し
+        //緯度経度を取得する
         navigator.geolocation.getCurrentPosition(function(position) {
-            initMap(position);
+            if(syncerWatch.map == null){
+                //mapを初期化
+                initMap(position);
+            }else{
+                updateMap(position);
+            }
         }, function(error) {
             // エラー番号に対応したメッセージ
             var errorInfo = ["原因不明のエラーが発生しました…。", "位置情報の取得が許可されませんでした…。", "電波状況などで位置情報が取得できませんでした…。", "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"];
@@ -95,4 +98,16 @@ function getGeolocation() {
         // アラート表示
         alert(errorMessage);
     }
+}
+
+//住所から緯度と経度を取得する
+function getLatLngFromAddress(address){
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function(results, status){
+        if(status == google.maps.GeocoderStatus.OK) {
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+            console.log("緯度:"+lat+"経度:"+lng);
+        }
+    });
 }
